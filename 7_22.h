21cfecb39b45593bdd262a06991551c9daaff256
zhueh <zhueh1991@sina.com>
2016-05-12 22:10:17 +0800

    7_22.h
    the answer of exercise 7.22

---
+++ 7_22.h
@@ -0,0 +1,31 @@
+#ifndef CP5_EX_7_15_H
+#define CP5_EX_7_15_H
+#include<string>
+#include<iostream>
+using std::string;
+istream &read(istream&, Person&);
+ostream &print(ostream&, Person&);
+class Person{
+public:
+	Person() = default;
+	Person(const string& sn, const string& sa) :name(sn), address(sa){};
+	Person(istream&);
+	const string& getName() const{ return name; };
+	const string& getAddress() const{ retrun address; };
+private:
+	string name;
+	string address;
+
+};
+Person::Person(istream &is){
+	read(is, *this);
+}
+istream &read(istream &is, Person &item){
+	is >> item.name >> item.address;
+	return is;
+}
+ostream &print(ostream& os, const Person& item){
+	os << item.name << " " << item.address;
+	return os;
+}
+#endif
