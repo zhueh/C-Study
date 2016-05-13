b12ee525a7337fe695935485ea36d9f8cea8b801
zhueh <zhueh1991@sina.com>
2016-05-13 11:43:55 +0800

    exercise 7.27

--- 7_27.h
+++ 7_27.h
@@ -1,6 +1,9 @@
 #ifndef CP5_EX_7_23_H
 #define CP5_EX_7_23_H
-
+#include<iostream>
+#include<string>
+using std::string;
+using std::cout; using std::cin; using std::endl;
 class Screen{
 public:
 	typedef string::size_type pos;
@@ -11,14 +14,14 @@ public:
 	char get(pos r, pos c) const{ return contents[r*width + c]; }
 	Screen &move(pos r, pos c);
 	Screen &set(char);
-	const Screen &display(ostream &os) const { do_display(os); return *this; }
-	Screen &display(ostream&){ do_display(os); return *this; }
+	const Screen &display(std::ostream &os) const { do_display(os); return *this; }
+	Screen &display(std::ostream &os){ do_display(os); return *this; }
 
 private:
 	pos height = 0, width = 0;
 	pos cursor = 0;
 	string contents;
-	void do_display(ostream &os) const { os << contents; }
+	void do_display(std::ostream &os) const { os << contents; }
 };
 Screen &Screen::move(pos r, pos c){
 	cursor = r*width + c;
