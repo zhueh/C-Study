f53681f330683e1d336b59d80ecc45683c783f2e
zhueh <zhueh1991@sina.com>
2016-05-13 11:45:25 +0800

    exercise 7.24

--- 7_24.h
+++ 7_24.h
@@ -1,6 +1,9 @@
-#ifndef CP5_EX_7_23_H
-#define CP5_EX_7_23_H
-
+#ifndef CP5_EX_7_24_H
+#define CP5_EX_7_24_H
+#include<iostream>
+#include<string>
+using std::string;
+using std::cout; using std::cin; using std::endl;
 class Screen{
 public:
 	typedef string::size_type pos;
@@ -8,12 +11,13 @@ public:
 	Screen(pos ht, pos wd, char c) :height(ht), width(wd), contents(ht*wd, c){}
 	Screen(pos ht, pos wd) :height(ht), width(wd), contents(ht*wd, ' '){}
 	char get() const{ return contents[cursor]; }
-	Screen move(pos r, pos c);
+	char get(pos r, pos c) const{ return contents[r*width + c]; }
+
 private:
 	pos height = 0, width = 0;
 	pos cursor = 0;
 	string contents;
-	
+
 };
 
 #endif
